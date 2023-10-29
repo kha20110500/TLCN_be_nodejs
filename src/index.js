45f42e1778f2b93d6567b8f5c6420c9f8150de23
kha20110500 <khareal2016@gmail.com>
2023-10-29 16:07:07 +0700

    Initial commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,35 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require("express");
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
 
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
+const UserRouter = require("./Route/UserRouter");
+const PaymentRouter = require("./Route/PaymentRouter");
+const OrderRouter = require("./Route/OrderRouter");
+const ProductRouter = require("./Route/ProductRouter");
+const multer = require("multer");
+const mongo = require("./mongo");
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3001;
+// Middleware để xử lý dữ liệu form-data
+const upload = multer();
+app.use(cors());
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ limit: "50mb" }, { extends: false }));
+app.use(express.json());
+app.use(cookieParser());
+
+app.use("/api/user", UserRouter);
+app.use("/api/product", ProductRouter);
+app.use("/api/order", OrderRouter);
+app.use("/api/payment", PaymentRouter);
+
+mongo.connect();
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
-})
+    console.log("Server is running in port: ", +port);
+});
